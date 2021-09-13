218bf8bfc791af631dc0c6dafecd83e3258f449d
SyHoMadara <79520000+SyHoMadara@users.noreply.github.com>
2021-09-13 13:24:23 +0430

    change tkinter filedialog to pyqt filedialog

--- CollectingWords/libs/file_tools.py
+++ CollectingWords/libs/file_tools.py
@@ -1,12 +1,18 @@
 import os
 from pathlib import Path
-from tkinter import filedialog, Tk
+from PyQt5.QtWidgets import QFileDialog
 
 
-class FileWithTkinter:
-    def __init__(self, file_type: list) -> None:
-        self.root = Tk()
-        self.root.withdraw()
-        self.base_file = filedialog.askopenfilename(filetypes=file_type)
-        self.file_name, self.file_extension = os.path.splitext(os.path.basename(self.base_file))
-        self.file_dir = Path(self.base_file).resolve().parent
+class AskFileGraphical:
+    def __init__(self, file_type: str, r_w: str) -> None:
+        try:
+            if r_w == "w":
+                self.base_file = QFileDialog.getSaveFileName(filter=file_type)
+            else:
+                self.base_file = QFileDialog.getOpenFileName(filter=file_type)[0]
+            self.file_name, self.file_extension = os.path.splitext(os.path.basename(self.base_file))
+            self.file_dir = Path(self.base_file).resolve().parent
+        except Exception as ex:
+            self.base_file = ""
+            self.file_name = ""
+            self.file_dir = ""
