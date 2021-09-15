3d9f417db6da7e9d4c721019af5e86c786427185
SyHoMadara <79520000+SyHoMadara@users.noreply.github.com>
2021-09-15 11:36:07 +0430

    fix problem

--- CollectingWords/libs/file_tools.py
+++ CollectingWords/libs/file_tools.py
@@ -7,7 +7,7 @@ class AskFileGraphical:
     def __init__(self, file_type: str, r_w: str) -> None:
         try:
             if r_w == "w":
-                self.base_file = QFileDialog.getSaveFileName(filter=file_type)
+                self.base_file = QFileDialog.getSaveFileName(filter=file_type)[0]
             else:
                 self.base_file = QFileDialog.getOpenFileName(filter=file_type)[0]
             self.file_name, self.file_extension = os.path.splitext(os.path.basename(self.base_file))
